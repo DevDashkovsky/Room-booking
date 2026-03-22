165d2d5f554c93d567689da831acaf0bbf98da03
Egor Dashkovskii <e.dashkovskii1@g.nsu.ru>
2026-03-22 13:29:58 +0700

    feat: init go.mod && add logger, db conn and point /_info in main

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,46 @@
-package api
+package main
 
-import "fmt"
+import (
+	"context"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/DevDashkovsky/room-booking/internal/config"
+	"github.com/DevDashkovsky/room-booking/internal/db"
+	"github.com/go-chi/chi/v5"
+	"github.com/rs/zerolog"
+)
 
 func main() {
-	fmt.Println("server starting")
+	logger := zerolog.New(os.Stdout).With().Timestamp().Logger()
+
+	cfg := config.Load()
+	ctx := context.Background()
+
+	var pool *db.Pool
+	for i := 0; i < 30; i++ {
+		p, err := db.Connect(ctx, cfg.DatabaseURL)
+		if err == nil {
+			pool = p
+			break
+		}
+		logger.Warn().Err(err).Msgf("waiting for db... (%d/30)", i+1)
+		time.Sleep(time.Second)
+	}
+	if pool == nil {
+		logger.Fatal().Msg("failed to connect to database")
+	}
+	defer pool.Close()
+
+	r := chi.NewRouter()
+
+	r.Get("/_info", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	})
+
+	logger.Info().Str("port", cfg.Port).Msg("starting server")
+	if err := http.ListenAndServe(":"+cfg.Port, r); err != nil {
+		logger.Fatal().Err(err).Msg("server failed")
+	}
 }
